8560dcb8b85eae263a3db5944bd1f28a33a45689
mateusz-ciepielowski <mateusz.ciepielowski1@interia.pl>
2025-01-19 23:42:02 +0100

    Server and client from the same address

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ import mongoose from "mongoose";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import bodyParser from "body-parser";
+import path from "path";
+import express from "express";
 
 import chatterRouter from "./routes/chatter.js";
 import messageRouter from "./routes/message.js";
@@ -12,6 +14,8 @@ import { app, server } from "./lib/socket.js";
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(process.env.MONGO_URL);
+
+const __dirname = path.resolve();
 
 app.use(bodyParser.json());
 app.use(
@@ -26,5 +30,13 @@ app.use(cookieParser());
 app.use("/api", chatterRouter);
 app.use("/api/message", messageRouter);
 app.use("/api/chatroom", chatroomRouter);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
 server.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
